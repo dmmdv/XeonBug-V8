8d106ddc8d287cd417ebfb1e467a054a71373009
dmmdv <152953911+dmmdv@users.noreply.github.com>
2024-10-14 02:55:46 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,13 +10,13 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "254708911515"
+global.ownernomer = "+254708911515"
 global.ownername = "Qasim Ali"
 global.ytname = "YT: GlobalTechInfo"
 global.socialm = "GitHub: GlobalTechInfo"
 global.location = "Pakistan, Karachi, Karachi"
 
-global.ownernumber = '254708911515'  //creator number
+global.ownernumber = '+254708911515'  //creator number
 global.ownername = 'Qasim Ali' //owner name
 global.botname = 'XeonBug-V8' //name of the bot
 
